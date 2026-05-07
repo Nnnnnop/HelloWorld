64d9bca1e5882b3f76e439ea875298a5c0900cc0
CrazyUOuOb <zcra84040@gmail.com>
2026-05-07 09:45:09 +0800

    Spring boot initializer

---
+++ IoTMINDs/IoTMINDs/src/main/java/com/IoTMINDs/IoTminDsApplication.java
@@ -0,0 +1,13 @@
+package com.IoTMINDs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTminDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTminDsApplication.class, args);
+	}
+
+}
---
+++ IoTMINDs/IoTMINDs/src/test/java/com/IoTMINDs/IoTminDsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IoTMINDs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTminDsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
